cf587c2014eaaf741aaf6917c6d10f42a6c76efa
Emmanuel Balogun <emmanx25@gmail.com>
2024-10-25 20:07:01 +0100

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    // float item1 = 20.4;
+    // float item2 = 3.4;
+    // float item3 = 12.67;
+
+    // printf("Item 1: %.3f\n", item1);
+    // printf("Item 2: %.3f\n", item2);
+    // printf("Item 3: %.3f\n", item3);
+
+    // return 0;
+
+    int age;
+
+    printf("\nHow old are you?: ");
+    scanf("%d", &age);
+
+    printf("Your are %d years old\n", age);
+}
